3e0b792cddd8d29e4e0939a95db69f69f1ad7ee1
Lisie001 <romualdoandriamamonjy5@gmail.com>
2022-05-07 16:40:16 +0300

    finale code

--- js/Game.js
+++ js/Game.js
@@ -49,3 +49,53 @@ tweeter.addEventListener('mouseover' , function () {
 tweeter.addEventListener('mouseout' , function () {
     tweeter.style.color= '#FFFFFF';
 });
+
+//fade in 
+let ratio = .1;
+
+let options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+  }
+  
+ let handelIntersect = function (entries , observer) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('visible-game')
+            observer.unobserve(entry.target)
+        } 
+    })
+ }
+let observer = new IntersectionObserver(handelIntersect, options);
+observer.observe(document.querySelector('.texte-titre'));
+observer.observe(document.querySelector('.niveau1'));
+observer.observe(document.querySelector('.qst'));
+observer.observe(document.querySelector('.har'));
+observer.observe(document.querySelector('.face'));
+observer.observe(document.querySelector('.il'));
+observer.observe(document.querySelector('.tim'));
+observer.observe(document.querySelector('.jack'));
+observer.observe(document.querySelector('.des'));
+observer.observe(document.querySelector('.le'));
+observer.observe(document.querySelector('.sa'));
+observer.observe(document.querySelector('.cat'));
+observer.observe(document.querySelector('.jon'));
+observer.observe(document.querySelector('.ville'));
+observer.observe(document.querySelector('.prod'));
+observer.observe(document.querySelector('.app'));
+observer.observe(document.querySelector('.inte'));
+observer.observe(document.querySelector('.noir'));
+
+//zoom image
+
+let handelIntersection = function (entries , observer) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('val-zoom')
+            observer.unobserve(entry.target)
+        } 
+    })
+ }
+let observer1 = new IntersectionObserver(handelIntersection, options);
+observer1.observe(document.querySelector('.val'))
